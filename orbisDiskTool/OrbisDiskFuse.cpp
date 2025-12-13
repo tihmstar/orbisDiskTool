49a24310fdec25d8c8d1846597da86cb300e106e
tihmstar <tihmstar@gmail.com>
2025-12-13 17:41:47 +0100

    linux compile fixes

--- orbisDiskTool/OrbisDiskFuse.cpp
+++ orbisDiskTool/OrbisDiskFuse.cpp
@@ -20,7 +20,7 @@
 using namespace orbisDiskTool;
 
 #ifdef HAVE_FUSE
-static int fs_getattr(const char *path, struct stat *stbuf) {
+static int fs_getattr(const char *path, struct stat *stbuf) noexcept{
      struct fuse_context *ctx = fuse_get_context();
      OrbisInternalDisk *disk = (OrbisInternalDisk*)ctx->private_data;
      int res = 0;
@@ -44,7 +44,7 @@ static int fs_getattr(const char *path, struct stat *stbuf) {
      return res;
  }
 
-static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi){
+static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) noexcept{
     (void) offset;
     (void) fi;
 
@@ -61,7 +61,7 @@ static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t
     return 0;
 }
 
-static int fs_open(const char *path, struct fuse_file_info *fi){
+static int fs_open(const char *path, struct fuse_file_info *fi) noexcept{
     struct fuse_context *ctx = fuse_get_context();
     OrbisInternalDisk *disk = (OrbisInternalDisk*)ctx->private_data;
     if(!path || !fi)
@@ -84,7 +84,7 @@ static int fs_open(const char *path, struct fuse_file_info *fi){
     return 0;
 }
 
-static int fs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi){
+static int fs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) noexcept{
     struct fuse_context *ctx = fuse_get_context();
     OrbisInternalDisk *disk = (OrbisInternalDisk*)ctx->private_data;
     if(!path || !buf)
@@ -102,7 +102,7 @@ static int fs_read(const char *path, char *buf, size_t size, off_t offset, struc
     }
 }
 
-static int fs_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi){
+static int fs_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi) noexcept{
     struct fuse_context *ctx = fuse_get_context();
     OrbisInternalDisk *disk = (OrbisInternalDisk*)ctx->private_data;
     // Check parameters
@@ -123,10 +123,10 @@ static int fs_write(const char *path, const char *buf, size_t size, off_t offset
 
 static const struct fuse_operations orbisDiskFuse_ops = {
     .getattr = fs_getattr,
-    .readdir = fs_readdir,
     .open    = fs_open,
     .read    = fs_read,
     .write   = fs_write,
+    .readdir = fs_readdir,
 };
 #endif
 
